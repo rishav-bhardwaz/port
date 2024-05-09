2bdb6682c98e2d61512df25ed4cc960616294d2a
rishav-bhardwaz <rishav76000@gmail.com>
2024-05-09 15:01:19 +0530

    cmt3

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@
     
   }
 
-  module.exports = nextConfig
+  module.exports = {
+    // Add other Next.js configuration options if needed
+    // For example, you can specify target: 'serverless' or other options here
+    output: {
+      // Set your desired output directory for the static HTML export
+      // You can change "build" to any directory name you prefer
+      dir: 'build',
+    },
+  };
